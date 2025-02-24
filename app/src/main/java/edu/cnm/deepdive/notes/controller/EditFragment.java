63f5cefde8194232d6cd3c58c05a73e733677718
David <dbarbour2@cnm.edu>
2025-02-24 11:55:27 -0700

    Prepare for google sign in

--- app/src/main/java/edu/cnm/deepdive/notes/controller/EditFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/EditFragment.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.notes.controller;
 
 import android.Manifest.permission;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -157,14 +158,15 @@ public class EditFragment extends BottomSheetDialogFragment {
   }
 
   private void capture(){
-    File captureDir = new File(requireContext().getFilesDir(),getString(R.string.capture_directory));
+    Context context = requireContext();
+    File captureDir = new File(context.getFilesDir(),getString(R.string.capture_directory));
     //noinspection ResultOfMethodCallIgnored
     captureDir.mkdir();
     File captureFile;
     do{
       captureFile = new File(captureDir, UUID.randomUUID().toString());
     } while(captureFile.exists());
-    Uri uri = FileProvider.getUriForFile(requireContext(), AUTHOPRITY, captureFile);
+    Uri uri = FileProvider.getUriForFile(context, AUTHOPRITY, captureFile);
     viewModel.setPendingCaptureUri(uri);
     captureLauncher.launch(uri);
   }
